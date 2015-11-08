7d0fd32180d3b7546254f4c8925d494f2b6e95d5
mehanig <mehanig@gmail.com>
2015-11-09 02:08:19 +0300

    command line options added

--- XMEMLops/run.py
+++ XMEMLops/run.py
@@ -5,7 +5,7 @@ from classes.BinNode import BinNode
 
 @click.command()
 @click.option('-i', help='Path to XML template file')
-@click.option('-o', help='Output file')
+@click.option('-o', default=None, help='Output file')
 def parse_course_bin_xml(i, o):
     xml_course = dom.parse(i)
     MainBin = BinNode(xml_course, mainNode=True)
@@ -15,7 +15,8 @@ def parse_course_bin_xml(i, o):
     # MainBin.get_node_stat()
     MainBin.montage_by_steps()
     # MainBin.montage_by_one()
-    MainBin.write_to_file()
+    MainBin.write_to_file(o)
+
 
 if __name__ == '__main__':
     parse_course_bin_xml()
