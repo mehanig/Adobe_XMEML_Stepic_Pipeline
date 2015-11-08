34265292d45a359c9002303b2cd79893276eebae
mehanig <mehanig@gmail.com>
2015-11-08 23:54:59 +0300

    smal fixes before refactoring

--- XMEMLops/__init__.py
+++ XMEMLops/__init__.py
@@ -15,7 +15,7 @@ DEFAULT_SUBSTEP_NAME_PATTERN = r'^Step(?P<substep_id>[0-9]+)from(?P<step_id>[0-9
 DEFAULT_SUBSTEP_FILES_PATTERN = r'^Step(?P<substep_id>[0-9]+)from(?P<step_id>[0-9]+)_(?P<postfix>Professor|Screen).'
 NAME_REPLACEMENT_LIST = ['file', 'clipitem', 'sequence']
 PARSED_NAMES = ['bin', 'clip', 'sequence']
-POSTFIX_PROF = "_Professor.TS"
+POSTFIX_PROF = "_Professor.mp4"
 POSTFIX_SCREEN = "_Screen.mp4"
 FILE_PATHURL_START = 'file:/'
 NEW_FILE_POSTFIX = '_new.xml'
@@ -34,7 +34,7 @@ def validate_folder(path):
     pass
 
 # generate data dict about course, finds lessons, course full path and steps
-# key 'seq' contains another dictionary with keys - relative lesson paths from course path, 
+# key 'seq' contains another dictionary with keys - relative lesson paths from course path,
 # and values - list of step files
 # First part with sys.args needed to split unnecessary parts of path, so we can run script
 # from any folder "pyton3 Path/To/This/run.py CoursePath"
@@ -54,7 +54,7 @@ def generate_sequence_list(path, extra_path_lvl=0):
         r = r.split(os.sep)
         l, stepName = r[-3:-1], r[-1]
         l = os.path.join(*l)
-        if s['seq'].get(l): 
+        if s['seq'].get(l):
             if not stepName in s['seq'][l]:
                 s['seq'][l].append(stepName)
         else:
@@ -110,7 +110,7 @@ def shift_node_time(seq, ticks):
             el.firstChild.nodeValue = int(el.firstChild.nodeValue) + ticks
 
 
-        
+
 # TODO: rewrite as class method?
 #Add <clipitems> from seq2 <media> to appropriate <tracks>
 def add_clip_to_end(seq1, seq2):
@@ -162,10 +162,10 @@ class BinNode(object):
             seen = set()
             seen_add = seen.add
             return [ x for x in seq if not (x in seen or seen_add(x))]
-        
+
         node.to_montage.append(add_to_montage)
         node.to_montage = make_unique(node.to_montage)
-        
+
 
     # Finds only nodes with name node and value of substep naming pattern
     # To avoid duplicates we only add half of nodes to montage (thouse which are not Screencasts)
@@ -192,9 +192,9 @@ class BinNode(object):
             path = n.firstChild.nodeValue
             #print('path', path)
             duration = calculate_duration(path)
-            self.file_links.update( {name : [file_id, duration]}) 
+            self.file_links.update( {name : [file_id, duration]})
             print(duration)
-            
+
 
     def parse(self):
         c = self.main.getElementsByTagName('children')
@@ -204,7 +204,7 @@ class BinNode(object):
         if children:
             for n in children.childNodes:
                 if n.nodeName in PARSED_NAMES:
-                    self._update_nodes_to_montage(n)                            
+                    self._update_nodes_to_montage(n)
                     newChild = BinNode(n, parent=self)
                     newChild.parse()
                     self.children.append(n)
@@ -214,7 +214,7 @@ class BinNode(object):
     # Uniquify = replace <file id=...> and <masterclipid>...</> with apropriate values
     # replace <duration> with new calculated duration
     # integer !usually! is equal , file-33 is from masterclip-33 and so on
-    
+
     def _uniquify_node_with_linked_files(self, node, linked_node_name):
 
         def get_screen_and_video_dict(linked_node_name):
@@ -235,6 +235,7 @@ class BinNode(object):
             clip_type = 'video'
             if ci.getElementsByTagName('name')[0].firstChild.nodeValue.endswith(POSTFIX_SCREEN):
                 clip_type = 'screen'
+
             for el in ci.getElementsByTagName('duration'):
                 el.firstChild.nodeValue = ops[clip_type][1][1]
             for el in ci.getElementsByTagName('end'):
@@ -242,7 +243,7 @@ class BinNode(object):
             for el in ci.getElementsByTagName('out'):
                 el.firstChild.nodeValue = ops[clip_type][1][1]
             for el in ci.getElementsByTagName('name'):
-                el.firstChild.nodeValue = linked_node_name 
+                el.firstChild.nodeValue = linked_node_name + '_' + clip_type
 
             for el in ci.getElementsByTagName('file'):
                 print(el.attributes['id'].value)
@@ -268,9 +269,9 @@ class BinNode(object):
             self._uniquify_node_with_linked_files(new_Seq, linked_node_name )
             main_bin.appendChild(new_Seq)
 
-    # Creates one sequence for 1 step folder based on subsptep_id 
+    # Creates one sequence for 1 step folder based on subsptep_id
     def montage_by_steps(self):
-    
+
         self.montage_by_one()
 
         substeps = defaultdict(dict)
@@ -281,7 +282,7 @@ class BinNode(object):
                 step_id = int(m.group('step_id'))
                 substep_id = int(m.group('substep_id'))
                 substeps[step_id][substep_id] = n
-    
+
         for index, substep_list in substeps.items():
             if len(substep_list.keys()) > 1:
                 print(substep_list.keys())
@@ -293,8 +294,8 @@ class BinNode(object):
     # Workaround to delete emply lines after minidom.toprettyxml
     # Supports only UTF-8
     def write_to_file(self, path = None):
-        
-        name = str(self.name) + NEW_FILE_POSTFIX 
+
+        name = str(self.name) + NEW_FILE_POSTFIX
         with open(name, 'w') as target:
             target.write(_new_prettify(self.main.toprettyxml()))
 
@@ -310,25 +311,26 @@ def parse_course_bin_xml(xml_path):
     MainBin = BinNode(xml_course, mainNode = True)
     MainBin.parse()
     print("FINAL!")
-    MainBin.get_node_stat()
-    MainBin.montage_by_steps()
+    # MainBin.get_node_stat()
+    # MainBin.montage_by_steps()
+    MainBin.montage_by_one()
     MainBin.write_to_file()
 
 # Based on sequence template, generate a lot of sequences with files from anothers steps
 # Useless while this approach not reuse project files with linking inside project
 def generate_template_from_list(tmplt, op_list, seqName = None):
-    
+
     dom_f = dom.parse(tmplt)
     to_file = dom.parse(tmplt)
-    
+
     def getAllTrackNodes(domfile):
         all_tracks = domfile.getElementsByTagName('track')
         return [t for t in all_tracks if len(t.getElementsByTagName('pathurl')) > 0]
 
     def construct_path(l):
         return os.sep.join(l)
-    
-    video_nodes = getAllTrackNodes(to_file) 
+
+    video_nodes = getAllTrackNodes(to_file)
     for path, items in op_list['seq'].items():
         to_file.getElementsByTagName('name')[0].firstChild.nodeValue = str(items[0]) + '_Comp'
         for item in items:
@@ -338,7 +340,7 @@ def generate_template_from_list(tmplt, op_list, seqName = None):
                 name = name_node.firstChild.nodeValue
                 postfix = name.split('_')[-1]
                 name_node.firstChild.nodeValue = str(items[0]) + "_" + postfix
-        
+
             for v in video_nodes:
                 node = v.getElementsByTagName('pathurl')[0].firstChild
                 new_path = construct_path([FILE_PATHURL_START, op_list['course_path'], str(path), str(item), str(item)])
@@ -346,7 +348,7 @@ def generate_template_from_list(tmplt, op_list, seqName = None):
                     node.nodeValue = new_path + POSTFIX_PROF
                 elif node.nodeValue.endswith(POSTFIX_SCREEN):
                     node.nodeValue = new_path + POSTFIX_SCREEN
-        name = str(item) + NEW_FILE_POSTFIX 
+        name = str(item) + NEW_FILE_POSTFIX
         target = open(name, 'w')
         to_file.writexml(target)
         target.close()
@@ -355,5 +357,5 @@ def generate_template_from_list(tmplt, op_list, seqName = None):
 if __name__ == '__main__':
     #x = generate_sequence_list('Algo2015Course')
     #generate_template_from_list('example_media.xml', x)
-    parse_course_bin_xml('Algo_2015_w2.xml')
+    parse_course_bin_xml('Algo_2015_w3.xml')
     #print("X:", x)
