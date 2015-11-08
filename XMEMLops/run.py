ddce40bb4fab34ad382c03fc9eed229a85b2b9f9
mehanig <mehanig@gmail.com>
2015-11-09 02:01:29 +0300

    CLI args added

--- XMEMLops/run.py
+++ XMEMLops/run.py
@@ -1,17 +1,21 @@
 import xml.dom.minidom as dom
-
+import click
 from classes.BinNode import BinNode
 
 
-def parse_course_bin_xml(xml_path):
-    xml_course = dom.parse(xml_path)
+@click.command()
+@click.option('-i', help='Path to XML template file')
+@click.option('-o', help='Output file')
+def parse_course_bin_xml(i, o):
+    xml_course = dom.parse(i)
     MainBin = BinNode(xml_course, mainNode=True)
     MainBin.parse()
     print("FINAL!")
+    print(o)
     # MainBin.get_node_stat()
     MainBin.montage_by_steps()
     # MainBin.montage_by_one()
     MainBin.write_to_file()
 
 if __name__ == '__main__':
-    parse_course_bin_xml('Algo_2015_w3.xml')
+    parse_course_bin_xml()
